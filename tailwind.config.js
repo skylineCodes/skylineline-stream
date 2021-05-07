8b39f989c04e7e6b807f4ce153707b24fc75926b
SkylineCodes <onakoyak@gmail.com>
2021-05-07 12:00:44 +0100

    Homepage completed

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      screens: {
+        "3xl": "2000px",
+      }
+    },
+  },
+  variants: {
+    extend: {
+      animation: ['group-hover'],
+      fontWeight: ['hover', 'focus'],
+    },
+  },
+  plugins: [require('tailwind-scrollbar-hide')],
+};
